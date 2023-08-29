ec81f086010ab9b5786804a2e51e8bfb10fb4a0c
jeffmanhart <jeffreymanhart@gmail.com>
2023-08-29 13:12:55 -0600

    misusing global variables

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Jeff'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    }
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+    }
+function overwriteBestCustomer() {
+    bestCustomer ='maybe bob'
+    return bestCustomer;
+    }
+    function changeLeastFavoriteCustomer() {
+        leastFavoriteCustomer ='ok it\'s bob'
+        return leastFavoriteCustomer;
+    }
